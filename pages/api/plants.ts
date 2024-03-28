a7c0ac81a91326c54c7167220333221cfc18f0cc
Kabindra Rijal <Rijalkabindra@gmail.com>
2024-03-28 12:58:12 +0545

    feat add plants API

---
+++ pages/api/plants.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ResponseData } from "@/typings/plantsType";
+
+const responseData: ResponseData = [
+  {
+    name: "Chinese Money",
+    price: "$22",
+    path: "/plant1.png",
+  },
+  {
+    name: "Nepali Money",
+    price: "$22",
+    path: "/plant3.png",
+  },
+  {
+    name: "Indian Money",
+    price: "$22",
+    path: "/plant4.png",
+  },
+  {
+    name: "Russian Money",
+    price: "$22",
+    path: "/pngeggplant6.png",
+  },
+];
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json(responseData);
+}
